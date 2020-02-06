5a32c5ba3856debb2468b9b2be81116a4859bb69
TanyaEf <efimenkot@gmail.com>
2020-02-06 18:36:33 +0200

    new components(styles)

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,6 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  template: `<p>warning-alert works!</p>`
+  template: `<p>warning-alert works!</p>`,
+  styles: [
+    `p {
+      padding: 20px;
+      background-color: mistyrose;
+      border: 1px solid red;
+    }`
+  ]
 })
 export class WarningAlertComponent {}
